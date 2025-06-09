8adbb3cc5bd774ac938fbc26d6c387cbe53546d5
todatoday <taki.souad-90@hotmail.com>
2025-06-09 15:15:36 +0200

    add pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 import Navbar from "../Navbar";
 
-
 const Header = () => {
     return(
         <header>
             <Navbar />
-        
         </header>
     )
 }
