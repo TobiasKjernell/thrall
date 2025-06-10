36fa5b8fb3d7bf993066fd8add79fe342a6b6028
todatoday <taki.souad-90@hotmail.com>
2025-06-10 20:14:08 +0200

    fixed pb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,10 @@ import Characters from "@/components/Characters";
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
       <HomeWelcomeSection />
       <Characters/>
       
-=======
  
->>>>>>> feature/01-headerNavbar
     </div>
   )
 }
