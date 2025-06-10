bffe67d8d3f4dda0a796cce94c82c8fa4cd59048
Yevhenii <ke1vin.kapustian@gmail.com>
2025-06-10 16:20:12 +0200

    Finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import HomeWelcomeSection from "@/components/HomeWelcomeSection";
-import StorylineSection from "@/components/StorylineSection";
 
 export default function Home() {
   return (
     <div>
       <HomeWelcomeSection />
-      <StorylineSection/>
     </div>
   );
 }
