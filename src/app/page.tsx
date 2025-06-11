2c6c231ff7e0589f324c99bd508021650f9d8469
PriyeshIllam <seeujob@gmail.com>
2025-06-11 22:54:15 +0200

    about the game component called in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HomeWelcomeSection from "@/components/HomeWelcomeSection";
 import Characters from "@/components/Characters";
 import StorylineSection from "@/components/StorylineSection";
+import AboutTheGame from "@/components/AboutTheGameSection";
 
 export default function Home() {
   return (
     <div>
       <HomeWelcomeSection />
+      <AboutTheGame />
       <Characters/>
       <StorylineSection/>
     </div>
