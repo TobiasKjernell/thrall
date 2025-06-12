cda1d9e49e69a8812099667737806569f7b50857
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-06-12 12:33:57 +0200

    demo fix

--- src/data/navbardata.ts
+++ src/data/navbardata.ts
@@ -1,7 +1,7 @@
 import { NavLink } from "@/types/type";
 
 export const navLinks: NavLink[] = [
-  { name: "Thrall", path: "/thrall" },
+  { name: "Thrall", path: "/" },
   { name: "About", path: "/about" },
   { name: "Play", path: "/play" },
   {name: "Team", path: "/team"}
