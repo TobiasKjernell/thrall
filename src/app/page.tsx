addb53061a5df28fa7aa746189f1cc00d2e24189
Yevhenii <ke1vin.kapustian@gmail.com>
2025-06-12 10:30:32 +0200

    Changed spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HomeWelcomeSection from "@/components/HomeWelcomeSection";
+import StorylineSection from "@/components/StorylineSection";
 
 export default function Home() {
   return (
     <div>
       <HomeWelcomeSection />
+      <StorylineSection/>
     </div>
   );
 }
