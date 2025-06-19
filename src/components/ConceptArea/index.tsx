5fbdc1e0d933deac9a70b3c7cd95bd2bda155cfe
Yevhenii <ke1vin.kapustian@gmail.com>
2025-06-19 15:57:58 +0200

    Fixed?

--- src/components/ConceptArea/index.tsx
+++ src/components/ConceptArea/index.tsx
@@ -6,12 +6,7 @@ const ConceptArea = () => {
             <h2 className="concept__title">Concept</h2>
             <p className="concept__text">For (16+) players who enjoy emergent mechanics and dynamic combat</p>
             <div className="concept__picture-grid">
-<<<<<<< HEAD
-                {Array.from({ length: 14 }).map((_, index) =>  
-                <img key={index} className='concept__picture-grid__item' src={`/FrameScreenshoot/${index}.png`} alt="yep" width={400} height={200} />)}
-=======
                 {Array.from({ length: 4 }).map((_, index) => <img className='concept__picture-grid__item' src={`concept${index}.png`} alt="yep" width={400} height={200} />)}
->>>>>>> parent of 6f1bd6b (Ready to merge into main)
             </div>
         </section>
     )
