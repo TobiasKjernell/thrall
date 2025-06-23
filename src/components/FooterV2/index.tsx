c22547c35462c2a1f867ccccc96b3e8a0ff66716
Ankitasingh88 <ankita.shibu.singh@gmail.com>
2025-06-23 20:20:38 +0200

    feature/40-yearfooter fixesyear

--- src/components/FooterV2/index.tsx
+++ src/components/FooterV2/index.tsx
@@ -4,23 +4,25 @@ import Logo from "../Logo"
 
 const FooterV2 = () => {
     const currentYear = new Date().getFullYear();
-
     return(
         <footer className="footer">
             <div className="footer__logo">
                 <Logo/>
             </div>
+
             <div className="footer__description">
                 <div className='footer__description--about'>
                     <p><strong>Genres:</strong> Combat · Light Strategy · Narrative</p>
                     <p><strong>Target Experience:</strong> Emergent gameplay and creative player-facing tools/mechanics</p>
                 </div>
+
                 <h3 className="footer__description--title">© {currentYear} Copyright. Thrall. All rights reserved.</h3>
                 <div className="footer__description--items">
                     <a href="https://itch.io/docs/legal/terms">Terms and conditions</a>
                     <span>|</span>
                     <a href="https://itch.io/docs/legal/privacy-policy">Privacy policy</a>
                 </div>
+                
             </div>
         </footer>
     )
